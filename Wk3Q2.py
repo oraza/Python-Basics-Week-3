07af6a3a9cae92ecfb3c86bdede0a117ab9e4f9c
Owais Raza <reye27@gmail.com>
2020-07-27 02:01:01 +0500

    update
    update

--- Wk3Q2.py
+++ Wk3Q2.py
@@ -4,7 +4,10 @@ Created on Mon Jul 27 01:18:45 2020
 
 @author: Owais Raza
 """
-
+#The variable sentence stores a string. Write code to determine how many words 
+#in sentence start and end with the same letter, including one-letter words. 
+#Store the result in the variable same_letter_count.
+#Hard-coded answers will receive no credit.
 
 sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
 words = sentence.split(" ")
